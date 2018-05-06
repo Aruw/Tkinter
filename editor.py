1f88a5521cb0b1ff61e6b60ea048c6a96a4bba64
Aruw <andre_wada@hotmail.com>
2018-05-06 15:50:52 -0300

    Added some changes to the Editor!

--- editor.py
+++ editor.py
@@ -27,6 +27,13 @@ def abrir():
 def limpar():
     text.delete(1.0, END)
 
+def sobre():
+    sobre = Tk()
+    sobre.title('About')
+    sobre.resizable(False, False)#(X,Y)
+    Label(sobre, text='Simple editor.\n Free to use! xD\n Created by Aruw!', padx=20, pady=10).pack()
+    sobre.mainloop()
+
 fileMenu = Menu(menuBar, tearoff=0)
 fileMenu.add_command(label='Save', command=salvar)
 fileMenu.add_command(label='Open', command=abrir)
@@ -34,7 +41,7 @@ fileMenu.add_command(label='Clear', command=limpar)
 menuBar.add_cascade(label='File', menu=fileMenu)
 
 helpMenu = Menu(menuBar, tearoff=0)
-helpMenu.add_command(label='About')
+helpMenu.add_command(label='About', command=sobre)
 menuBar.add_cascade(label='Help', menu=helpMenu)
 
 frame = Frame(root)
